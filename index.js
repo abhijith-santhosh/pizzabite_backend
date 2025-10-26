d1092cf47ca1b9963b1d12181f2108e70f23284f
abhijith-santhosh <abhijithsanthosh101@gmail.com>
2025-10-26 20:56:16 +0530

    React CORS err solved  from backend

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const cors = require("cors"); 
 const app = express();
 const PORT = 5000;
+
 // Import DB connection
 const db = require("./config/db");
 
 // Import routes
 const productRoutes = require("./routes/productRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
+
 // Middleware
 app.use(express.json());
 
+// CORS middleware
+app.use(cors({
+  origin: "http://localhost:5173", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true 
+}));
 
 // Default route
 // app.get("/", (req, res) => {
@@ -18,15 +27,7 @@ app.use(express.json());
 
 // Use routes
 app.use("/api/products", productRoutes);
-
 app.use("/api/categories", categoryRoutes); 
-
-
-
-
-
-
-
 
 // Start server
 app.listen(PORT, () => {
